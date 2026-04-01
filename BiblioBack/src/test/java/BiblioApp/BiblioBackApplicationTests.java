80608fd0aabf585a53f8c38c93a4ee6b6a5981e3
bguiguen56 <bastien.guiguen@gmail.com>
2026-04-01 15:02:57 +0200

    feat: init project with Spring Boot backend and Angular frontend

---
+++ BiblioBack/src/test/java/BiblioApp/BiblioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package BiblioApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
